4420d21e681f4bb85351af380d5469bb7d74a500
ali-shahshank <sploitfy@gmail.com>
2026-08-29 19:40:22 +0500

    MAJOR -/ UI update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,63 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Schibsted_Grotesk, Martian_Mono, Geist } from 'next/font/google';
 import './globals.css';
+import { cn } from '@/lib/utils';
+import LightRays from '@/components/LightRays';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
+
+const schibstedGorotesk = Schibsted_Grotesk({
+  variable: '--font-schibsted-sans',
   subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const martianMono = Martian_Mono({
+  variable: '--martian-mono',
   subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: 'Next.js Mastery',
-  description:
-    'Next.js application with TypeScript, Tailwind CSS, and NextAuth.js',
+  title: 'DevEvent',
+  description: 'Event Manager app for developer events',
 };
 
-export default function RootLayout({ children }: LayoutProps<'/'>) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={cn(
+        'h-full',
+        'antialiased',
+        schibstedGorotesk.variable,
+        martianMono.variable,
+        'font-sans',
+        geist.variable,
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
+          <LightRays
+            raysOrigin="top-center-offset"
+            raysColor="#5dfeca"
+            raysSpeed={0.5}
+            lightSpread={0.9}
+            rayLength={1.4}
+            followMouse={true}
+            mouseInfluence={0.02}
+            noiseAmount={0}
+            distortion={0.01}
+            className="custom-rays"
+            pulsating={false}
+            fadeDistance={1}
+            saturation={1}
+          />
+        </div>
+        <main>{children}</main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,15 @@
 const Home = () => {
   console.log('I am a server component');
-  return <main>Home Page</main>;
+  return (
+    <main>
+      <section>
+        <h1>Full-stack Next.js Event App</h1>
+        <p className="text-center mt-5">
+          Hackathons, meetups and conferences all in one place.
+        </p>
+      </section>
+    </main>
+  );
 };
 
 export default Home;
