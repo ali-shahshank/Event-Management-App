00f4d08b80416562534ca1a22ce385dbeaeca7df
ali-shahshank <sploitfy@gmail.com>
2026-09-02 10:39:26 +0500

    MINOR -/ Installed and integrated cloudinary

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: 'res.cloudinary.com' }],
+  },
 };
 
 export default nextConfig;
